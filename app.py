0941dd98443f90d645276d408d993703cf7bac7c
askyel <ariel.s.levy1@gmail.com>
2017-07-01 01:02:22 -0400

    flask templates

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__ == "__main__":
+   	app.debug = True
+	app.run(host='0.0.0.0',port=8000)
